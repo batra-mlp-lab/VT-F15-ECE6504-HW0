aee77f8b9fe9675486b08e49b97e95e66a9a7489
Abhishek Das <das.abhshk@gmail.com>
2015-08-18 15:30:20 +0530

    Homework 0 changes

--- spearmint-tuner.py
+++ spearmint-tuner.py
@@ -6,26 +6,26 @@ import numpy as np
 
 # import whichever classifier you need: LinearSVM/Softmax
 # Uncomment below line for SVM
-#from f15ece6504.classifiers import LinearSVM
+# from f15ece6504.classifiers import LinearSVM
 # Uncomment below line for Softmax
-#from f15ece6504.classifiers import Softmax
+# from f15ece6504.classifiers import Softmax
 
-# see load_cifar10_tvt.py in the hw0/ folder. This code helps you get your data in a single shot! 
-# This is basically a function that has all the steps you did in the ipython notebook to ready your data 
+# see load_cifar10_tvt.py in the hw0/ folder. This code helps you get your data in a single shot!
+# This is basically a function that has all the steps you did in the ipython notebook to ready your data
 # for the classifier
 from load_cifar10_tvt import load_cifar10_train_val
 
 def get_valError(learning_rate,reg):
-	
+
 	# load data
 	X_train,y_train,X_val,y_val = load_cifar10_train_val()
 	# init the classifier you need
-	
+
 	# Uncomment below line for SVM
-	# classifier = LinearSVM() 
-	
-	# Uncomment below line for SVM
-	# classifier = Softmax() 
+	# classifier = LinearSVM()
+
+	# Uncomment below line for Softmax
+	# classifier = Softmax()
 
 	# train classifier
 	loss_hist = classifier.train(X_train, y_train, learning_rate=1e-7, reg=5e4,num_iters=1500, verbose=True)
@@ -35,12 +35,12 @@ def get_valError(learning_rate,reg):
 	# return error rate
 	return (1 - val_accuracy)
 
-# Write a function like this called 'main'! 
+# Write a function like this called 'main'!
 def main(job_id, params):
     print 'Anything printed here will end up in the output directory for job #%d' % job_id
     print params
     return get_valError(params['learning_rate'], params['reg'])
-	
-	
+
+
 
 
