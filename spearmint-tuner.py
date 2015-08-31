b1235c01a9a53480f9d1414e723b54f679292056
Ashwin <asaavashwin@gmail.com>
2015-08-31 14:12:12 -0400

    changes to spearmint-tuner/svm.ipynb

--- spearmint-tuner.py
+++ spearmint-tuner.py
@@ -1,18 +1,20 @@
-# function that is called by spearmint! This calls the classifier and returns the value that needs to be minimized
+# function that is called by spearmint. This calls the classifier and returns the value that needs to be minimized
 # you may choose to return either the loss or the error on the validation set.
 
-# we always do this! :/
+# we always do this :/
 import numpy as np
 
 # import whichever classifier you need: LinearSVM/Softmax
+
 # Uncomment below line for SVM
 # from f15ece6504.classifiers import LinearSVM
 # Uncomment below line for Softmax
 # from f15ece6504.classifiers import Softmax
 
-# see load_cifar10_tvt.py in the hw0/ folder. This code helps you get your data in a single shot!
+# see load_cifar10_tvt.py in the hw0/ folder. This code helps you get your data in a single go. 
 # This is basically a function that has all the steps you did in the ipython notebook to ready your data
 # for the classifier
+
 from load_cifar10_tvt import load_cifar10_train_val
 
 def get_valError(learning_rate,reg):
@@ -28,14 +30,14 @@ def get_valError(learning_rate,reg):
 	# classifier = Softmax()
 
 	# train classifier
-	loss_hist = classifier.train(X_train, y_train, learning_rate=1e-7, reg=5e4,num_iters=1500, verbose=True)
+	loss_hist = classifier.train(X_train, y_train, learning_rate, reg, num_iters=1500, verbose=True)
 	# get validation error
 	y_val_pred = classifier.predict(X_val)
         val_accuracy = np.mean(y_val == y_val_pred)
 	# return error rate
 	return (1 - val_accuracy)
 
-# Write a function like this called 'main'!
+# The main function interfaces the above function with spearmint. 
 def main(job_id, params):
     print 'Anything printed here will end up in the output directory for job #%d' % job_id
     print params
